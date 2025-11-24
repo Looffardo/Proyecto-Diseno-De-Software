db3d1389e9a07146c4e34e6677a1a52d7b5789a6
Looffardo <christianlooffh@gmail.com>
2025-11-24 20:30:54 -0300

    a

--- mi-restaurante/backend/server.js
+++ mi-restaurante/backend/server.js
@@ -13,10 +13,16 @@ mongoose.connect("mongodb://127.0.0.1:27017/restaurante")
   .then(() => console.log("✅ Conectado a MongoDB"))
   .catch((err) => console.error("❌ Error conectando a MongoDB", err));
 
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Montamos las rutas
+app.use((req, res, next) => {
+  console.log(`➡️ ${req.method} ${req.url}`);
+  next();
+});
+
+
+// Rutas
 app.use("/api/platos", platosRouter);
 app.use("/api/pedidos", pedidosRouter);
 
