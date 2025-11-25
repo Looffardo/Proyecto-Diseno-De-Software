f760567535bdcae0ab939720b50caab1e2dfddf9
Looffardo <christianlooffh@gmail.com>
2025-11-25 03:14:42 -0300

    Last try

--- mi-restaurante/backend/server.js
+++ mi-restaurante/backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 4000;
 
 // MongoDB
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("✅ Conectado a MongoDB"))
   .catch((err) => console.error("❌ Error conectando a MongoDB", err));
 
